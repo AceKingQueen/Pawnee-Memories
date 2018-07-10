f906a432c2fbe19bd8d3c49f5526af289e685943
Allison <allisonkingwilliams@gmail.com>
2018-07-10 17:45:27 -0400

    no errors and work is needed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,76 @@
+//add click alert for each image and display total clicks
+  //add
+//check to see if clicked score is 0 
+  //if =0, add one point to total
+  //if !== 0, game over animate 
+    //reset score to zero
+    //check high score and compare
+      //if recentScore > high score, display it
+      //else do nothing
+//put images in random order after being clicked
+
 import React, { Component } from 'react';
 import PawneeCard from "./components/PawneeCard";
 import pawnee from "./pawnee.json";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
+// import NavPills from "./components/NavPills";
 import './App.css';
 
 class App extends Component {
 
   //state used will be imported from pawnee array
   state = {
-    pawnee
+    topScore: 0,
+    curScore: 0,
+    pawnee: pawnee,
+    unclickedCharacters: pawnee
+  };
+
+  // componentDidMount() {
+
+  // }
+
+  shuffleArray = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  handleClick = name => {
+     const clickCharacter = this.state.unclickedCharacters.find(item => item.name === name);
+
+     if (clickCharacter === undefined) {
+       this.setState({
+        message: "You guessed incorrectly!",
+        topScore: (this.state.curScore > this.state.topScore) ? this.state.curScore : this.state.topScore,
+        curScore: 0,
+        pawnee: pawnee,
+        unclickedCharacters: pawnee
+       });
+     } else {
+       const newCharacters = this.state.unclickedCharacters.filter(item => item.name !== name);
+
+       this.setState({
+         message: "Good Work!",
+         curScore: this.state.curScore + 1,
+         pawnee: pawnee,
+         unclickedCharacters: newCharacters
+       });
+     }
+
+      this.shuffleArray(pawnee);
   };
 
 
   render() {
     return (
       <Wrapper>
+        message={this.state.message}
+        curScore={this.state.curScore}
+        topScore={this.state.topScore}
+      
       <Title>Welcome to Pawnee
       <h3>Click an image to begin!</h3>
       <p className="App-intro">
@@ -24,9 +79,11 @@ class App extends Component {
       </Title>  
       {this.state.pawnee.map(character => (
         <PawneeCard 
-        id={character.id}
-        key={character.id}
+        handleClick={this.handleClick}
         image={character.image}
+        name={character.name}
+        selectCharacter={this.state.selectCharacter}
+        curScore={this.state.curScore}
         />
       ))}
       <footer></footer>
--- src/components/PawneeCard/PawneeCard.js
+++ src/components/PawneeCard/PawneeCard.js
@@ -4,10 +4,12 @@ import "./PawneeCard.css";
 const PawneeCard = props => (
     <div className="card">
       <div className="img-container">
-        <img alt={props.name} src={props.image} />
+      <a onClick={() => props.selectCharacter(props.name)} 
+                className={props.curScore === 0 ? "style_prevu_kit style_prevu_kit_ex" : "style_prevu_kit"}
+            >
+                <img alt={props.name} src={props.image} />
+            </a>
       </div>
-      <div className="content">
-    </div>
     </div>
   );
   
